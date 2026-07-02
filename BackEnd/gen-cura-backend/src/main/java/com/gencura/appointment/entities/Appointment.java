a57f7b9553a7902ebb5f3687d06beeb1f85423a6
Sudhanshu Dixit <sudhanshudix27@gmail.com>
2026-07-02 16:31:32 +0530

    feat(appointment): implement Appointment entity

---
+++ BackEnd/gen-cura-backend/src/main/java/com/gencura/appointment/entities/Appointment.java
@@ -0,0 +1,61 @@
+package com.gencura.appointment.entities;
+
+import java.time.LocalDateTime;
+
+import com.gencura.common.entities.BaseEntity;
+import com.gencura.common.enums.AppointmentStatus;
+import com.gencura.doctor.entities.Doctor;
+import com.gencura.patient.entities.Patient;
+import com.gencura.user.entities.User;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "appointments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name="appointment_id"))
+public class Appointment extends BaseEntity{
+	
+	@Column(name = "appointment_schedule", nullable = false)
+	private LocalDateTime appointmentSchedule;
+	
+	// GENERATED PER DAY. 
+	@Column(name="token_number",nullable = false)
+	private Integer tokenNumber;
+	
+	@Column(name = "chief_complaint", nullable = false)
+	private String chiefComplaint;
+	
+	@Column(name = "appointment_status", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private AppointmentStatus appointmentStatus = AppointmentStatus.BOOKED;
+	
+	@Column(length = 500)
+	private String remarks;
+	
+	// Relational
+	
+	@ManyToOne
+	@JoinColumn(name="user_id", nullable = false)
+	private User bookedBy;	
+	
+	@ManyToOne
+	@JoinColumn(name = "patient_id", nullable = false)
+	private Patient patient;
+	
+	@ManyToOne
+	@JoinColumn(name = "doctor_id", nullable = false)
+	private Doctor doctor;
+}
