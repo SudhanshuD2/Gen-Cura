0621d27a31b1817e9d9991e67ade26aa0f0a47fb
Sudhanshu Dixit <sudhanshudix27@gmail.com>
2026-07-27 15:22:31 +0530

    core entities completed

--- BackEnd/gen-cura-backend/src/main/java/com/gencura/common/entities/BaseEntity.java
+++ BackEnd/gen-cura-backend/src/main/java/com/gencura/common/entities/BaseEntity.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class BaseEntity {
+public abstract class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
---
+++ BackEnd/gen-cura-backend/src/main/java/com/gencura/user/entities/UserRole.java
@@ -0,0 +1,33 @@
+package com.gencura.user.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gencura.common.entities.BaseEntity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@AttributeOverride(name="id", column = @Column(name="user_id"))
+public class UserRole extends BaseEntity{
+	
+	@Pattern(regexp = "^ROLE_[A-Z_]+$", message = "Input must contain only uppercase letters and underscores")
+	private String role;
+	
+	@OneToMany(mappedBy = "role")
+	private List<User> users = new ArrayList<>();
+}
